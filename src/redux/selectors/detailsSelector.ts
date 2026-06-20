a5ee3ebf6c165e523dfe4eac1c985f64a7c57b57
Olamide Ojo <olamide.ojo@interswitchng.com>
2026-06-20 20:12:46 +0100

    detailsSelector

--- src/redux/selectors/detailsSelector.ts
+++ src/redux/selectors/detailsSelector.ts
@@ -4,27 +4,25 @@ import { RootState } from '../store';
 export const detailsSelector = (reduxState: RootState) => reduxState.details;
 
 export const selectMovieDetailsViewModel = (movieId: number) =>
-  createSelector(
-    (state: RootState) => state.details.cache[movieId],
-    data => {
-      if (!data?.movie) return null;
+  createSelector([detailsSelector], details => {
+    const data = details.cache[movieId];
+    if (!data?.movie) return null;
 
-      const movie = data.movie;
-      const credits = data.credits;
+    const movie = data.movie;
+    const credits = data.credits;
 
-      return {
-        ...movie,
-        year: movie.release_date?.split('-')[0],
-        duration: movie.runtime
-          ? `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}m`
-          : '',
-        genreText: movie.genres.map(g => g.name),
+    return {
+      ...movie,
+      year: movie.release_date?.split('-')[0],
+      duration: movie.runtime
+        ? `${Math.floor(movie.runtime / 60)}h ${movie.runtime % 60}m`
+        : '',
+      genreText: movie.genres.map(g => g.name),
 
-        topCast: credits?.cast ?? [],
-        director: credits?.crew.find(c => c.job === 'Director')?.name ?? '',
-        writer:
-          credits?.crew.find(c => c.job === 'Writer' || c.job === 'Screenplay')
-            ?.name ?? '',
-      };
-    },
-  );
+      topCast: credits?.cast ?? [],
+      director: credits?.crew.find(c => c.job === 'Director')?.name ?? '',
+      writer:
+        credits?.crew.find(c => c.job === 'Writer' || c.job === 'Screenplay')
+          ?.name ?? '',
+    };
+  });
